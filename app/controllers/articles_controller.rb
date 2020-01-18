2bed216ab3db702dd837e101ecdcf181777d8554
codershona <falgunimst95@gmail.com>
2020-01-18 19:46:54 +0100

    listing articles and index added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,36 @@
 class ArticlesController < ApplicationController
 
+  def index
+   @articles = Article.all
+  end
 
   def new
    @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
-    	flash[:notice] = "Article was successfully created"
+    	flash[:notice] = "Article has been successfully created"
     redirect_to article_path(@article)
     else
 	render 'new'
       end
   end
 
+  def update
+    @article = Article.find(params[:id])
+     if @article.update(article_params)
+      flash[:notice] = "Article has been successfully updated"
+       redirect_to article_path(@article)
+     else
+      render 'edit'
+    end
+   end
 
 def show
    @article = Article.find(params[:id])
