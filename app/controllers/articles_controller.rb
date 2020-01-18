74acb069f9a62a311909e642000c3cd2eeebf97c
codershona <falgunimst95@gmail.com>
2020-01-18 20:30:31 +0100

    Added partials and destroy option action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,13 @@ def show
    @article = Article.find(params[:id])
 end
 
+  def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  flash[:notice] = "Article has been successfully deleted"
+  redirect_to articles_path
+  end
+
   private
 
   def article_params
