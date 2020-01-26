4500f7df9a3e3b083d5a49fa4584ca7876aae6f0
codershona <falgunimst95@gmail.com>
2020-01-26 19:04:16 +0100

    Complete authentiation are working now very well, no errors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,8 +22,8 @@ class ApplicationController < ActionController::Base
 	def require_user
 
      
-			flash[:danger] = "You must be logged in to perform that action"
-         redirect_to root_path
+			# flash[:danger] = "You must be logged in to perform that action"
+   #       redirect_to root_path
      end
 
 	end
